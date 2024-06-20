77102588ec4e618a14071d34af387a78cd001df2
malslapq <malslapq@gmail.com>
2024-06-20 21:04:41 +0900

    feat : RedisConfig 파일 추가

---
+++ src/main/java/com/pyumpyum/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.pyumpyum.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory getRedisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(getRedisConnectionFactory());
+        return redisTemplate;
+    }
+
+
+}
