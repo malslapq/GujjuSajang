5b1cc0c08361253b7e53e1c298ef86fed298bdb2
malslapq <malslapq@gmail.com>
2024-06-20 15:48:45 +0900

    first commit

---
+++ src/main/java/com/pyumpyum/PyumPyumApplication.java
@@ -0,0 +1,13 @@
+package com.pyumpyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyumPyumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PyumPyumApplication.class, args);
+    }
+
+}
