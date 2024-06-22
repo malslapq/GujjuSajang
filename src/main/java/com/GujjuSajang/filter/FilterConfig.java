ddac43c8f083b0fd1097b92420a0ab75b3a823ff
malslapq <malslapq@gmail.com>
2024-06-22 11:05:08 +0900

    chore : 사용하지 않는 import 제거

--- src/main/java/com/GujjuSajang/filter/FilterConfig.java
+++ src/main/java/com/GujjuSajang/filter/FilterConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 @Configuration
 @RequiredArgsConstructor
