4c6cd7671d3362811d4dc1a9d5e36ea324a9ecbb
malslapq <malslapq@gmail.com>
2024-06-24 19:30:03 +0900

    feat : ProductDetailDto 추가

---
+++ src/main/java/com/GujjuSajang/product/dto/ProductDetailDto.java
@@ -0,0 +1,31 @@
+package com.GujjuSajang.product.dto;
+
+import com.GujjuSajang.product.entity.Product;
+import com.GujjuSajang.product.stock.Entity.Stock;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ProductDetailDto {
+
+    private Long id;
+    private Long sellerId;
+    private String name;
+    private int price;
+    private String description;
+    private int count;
+
+    public static ProductDetailDto from(Product product, Stock stock) {
+        return ProductDetailDto.builder()
+                .id(product.getId())
+                .sellerId(product.getSellerId()) // Assuming seller is an entity
+                .name(product.getName())
+                .price(product.getPrice())
+                .description(product.getDescription())
+                .count(stock.getCount())
+                .build();
+    }
+}
